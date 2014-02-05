1f915b93e7c20770f49be39e270cf111b06169b8
obratim <none@none>
2014-02-05 15:25:59 +0400

    API update

--- SqlWorker/SqlWorker.cs
+++ SqlWorker/SqlWorker.cs
@@ -29,47 +29,66 @@ namespace SqlWorker
 
         #region send files
 
-        public void InsertFile(
+        public delegate SqlFileStream FileStreamService();
+
+        public int InsertFileNoStoredProcs(
             String tableName,
             String fileIdFieldName, String fileDataFieldName,
             Dictionary<String, Object> attributes,
             System.IO.Stream inputStream,
-            long inputLength,
-            long bufLength = 512*1024,
-            String procName = null, int procFilePathIndex = 0, int procFileTokenIndex = 1)
+            long bufLength = 512*1024
+            //, String procName = null, int procFilePathIndex = 0, int procFileTokenIndex = 1
+        )
         {
-            Guid fileId;
-            if (!attributes.ContainsKey(fileIdFieldName))
-            {
-                fileId = Guid.NewGuid();
-                attributes.Add(fileIdFieldName, fileId);
-            }
-            else fileId = (Guid)attributes[fileIdFieldName];
-
-            InsertValues(tableName, attributes);
-
-            SqlFileStream sfs = null;
-
-            bool procNameFlag = !String.IsNullOrEmpty(procName); //check if procname was set
-            if (!procNameFlag) procName = "SELECT " + fileDataFieldName + ".PathName() as Path, GET_FILESTREAM_TRANSACTION_CONTEXT as Context from " + tableName + " where " + fileIdFieldName + " = @" + fileIdFieldName;
-            GetStructFromDB<bool>(procName,
-                new SqlParameter(fileIdFieldName, fileId),
-                reader =>
+            return InsertFileGeneric(inputStream,
+                () =>
                 {
-                    SqlDataReader dr = (SqlDataReader)reader;
-                    dr.Read();
-                    sfs = new SqlFileStream(dr.GetSqlString(0 + (procNameFlag ? procFilePathIndex : 0)).Value, dr.GetSqlBinary(0 + (procNameFlag ? procFilePathIndex : 1)).Value, System.IO.FileAccess.Write);
-                    return true;
-                });
+                    Guid fileId;
+                    if (!attributes.ContainsKey(fileIdFieldName))
+                    {
+                        fileId = Guid.NewGuid();
+                        attributes.Add(fileIdFieldName, fileId);
+                    }
+                    else fileId = (Guid)attributes[fileIdFieldName];
+
+                    InsertValues(tableName, attributes);
+
+                    //bool procNameFlag = !String.IsNullOrEmpty(procName); //check if procname was set
+                    //if (!procNameFlag)
+                    String procName = "SELECT " + fileDataFieldName + ".PathName() as Path, GET_FILESTREAM_TRANSACTION_CONTEXT as Context from " + tableName + " where " + fileIdFieldName + " = @" + fileIdFieldName;
+
+                    return GetStructFromDB<SqlFileStream>(procName,
+                        new SqlParameter(fileIdFieldName, fileId),
+                        reader =>
+                        {
+                            SqlDataReader dr = (SqlDataReader)reader;
+                            dr.Read();
+                            return new SqlFileStream(dr.GetSqlString(0).Value, dr.GetSqlBinary(1).Value, System.IO.FileAccess.Write);
+                        });
+                },
+                bufLength);
+        }
+
+        public int InsertFileGeneric(System.IO.Stream inputStream, FileStreamService InsertDataAndReturnSQLFileStream, long bufLength = 512*1024)
+        {
+            bool toCloseTranFlag = TransactionIsOpened;
+            if (!TransactionIsOpened) TransactionBegin();
+
+            var sfs = InsertDataAndReturnSQLFileStream();
 
             byte[] buffer = new byte[bufLength];
             int readen = inputStream.Read(buffer, 0, buffer.Length);
+            int writen = readen;
             while (readen > 0)
             {
                 sfs.Write(buffer, 0, readen);
                 readen = inputStream.Read(buffer, 0, buffer.Length);
+                writen += readen;
             }
             sfs.Close();
+
+            if (toCloseTranFlag) TransactionCommit();
+            return writen;
         }
 
         #endregion
