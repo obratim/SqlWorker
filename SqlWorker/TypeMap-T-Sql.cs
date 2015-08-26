894a548a598123e8f45b95ab2ba690a4d67bf697
obratim <none@none>
2015-08-26 15:59:05 +0300

    cs Type - sql type mappings

---
+++ SqlWorker/TypeMap-T-Sql.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace SqlWorker
+{
+    public abstract partial class ASqlWorker<TPC> where TPC : AbstractDbParameterConstructors, new()
+    {
+        protected static readonly Dictionary<Type, SqlDbType> typeMap_TSQL = new Dictionary<Type, SqlDbType>() {
+	        { typeof(byte) , SqlDbType.TinyInt },
+	        //{ typeof(sbyte) , SqlDbType.SByte },
+	        { typeof(short) , SqlDbType.SmallInt },
+	        //{ typeof(ushort) , SqlDbType.UInt16 },
+	        { typeof(int) , SqlDbType.Int },
+	        //{ typeof(uint) , SqlDbType.UInt32 },
+	        { typeof(long) , SqlDbType.BigInt },
+	        //{ typeof(ulong) , SqlDbType.UInt64 },
+	        { typeof(float) , SqlDbType.Float },
+	        { typeof(double) , SqlDbType.Real },
+	        { typeof(decimal) , SqlDbType.Decimal },
+	        { typeof(bool) , SqlDbType.Bit },
+	        { typeof(string) , SqlDbType.NVarChar },
+	        { typeof(char) , SqlDbType.NChar },
+	        { typeof(Guid) , SqlDbType.UniqueIdentifier },
+	        { typeof(DateTime) , SqlDbType.DateTime },
+	        { typeof(DateTimeOffset) , SqlDbType.DateTimeOffset },
+	        { typeof(byte[]) , SqlDbType.VarBinary },
+	        { typeof(byte?) , SqlDbType.TinyInt },
+	        //{ typeof(sbyte?) , SqlDbType.SByte },
+	        { typeof(short?) , SqlDbType.SmallInt },
+	        //{ typeof(ushort?) , SqlDbType.UInt16 },
+	        { typeof(int?) , SqlDbType.Int },
+	        //{ typeof(uint?) , SqlDbType.UInt32 },
+	        { typeof(long?) , SqlDbType.BigInt },
+	        //{ typeof(ulong?) , SqlDbType.UInt64 },
+	        { typeof(float?) , SqlDbType.Float },
+	        { typeof(double?) , SqlDbType.Real },
+	        { typeof(decimal?) , SqlDbType.Decimal },
+	        { typeof(bool?) , SqlDbType.Bit },
+	        { typeof(char?) , SqlDbType.NChar },
+	        { typeof(Guid?) , SqlDbType.UniqueIdentifier },
+	        { typeof(DateTime?) , SqlDbType.DateTime },
+	        { typeof(DateTimeOffset?) , SqlDbType.DateTimeOffset },
+            { typeof(TimeSpan), SqlDbType.Timestamp },
+            { typeof(TimeSpan?), SqlDbType.Timestamp },
+        };
+
+    }
+}
