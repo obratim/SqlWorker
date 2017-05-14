a544fddce22ebe9a90a29bf5c6fcfcfdc9626050
obratim <none@none>
2017-05-14 23:06:36 +0300

    checking creation and destruction of resources
    --HG--
    branch : debug_messages

--- SqlWorker/ASqlWorker.cs
+++ SqlWorker/ASqlWorker.cs
@@ -6,6 +6,62 @@ using System.Linq;
 
 namespace SqlWorker
 {
+    public static class SqlHelper
+    {
+        public static DbDataReader MyExecuteReader(this DbCommand cmd)
+        {
+            return new MyDataReader(cmd.ExecuteReader());
+        }
+    }
+
+    public class MyDataReader : DbDataReader
+    {
+        private DbDataReader reader;
+        public MyDataReader(DbDataReader basereader)
+        {
+            reader = basereader;
+        }
+        public override bool Read() { return reader.Read(); }
+        public override bool NextResult() { return reader.NextResult(); }
+        public override bool IsDBNull(int ordinal) { return reader.IsDBNull(ordinal); }
+        public override int GetValues(object[] values) { return reader.GetValues(values); }
+        public override object GetValue(int ordinal) { return reader.GetValue(ordinal); }
+        public override string GetString(int ordinal) { return reader.GetString(ordinal); }
+        public override long GetInt64(int ordinal) { return reader.GetInt64(ordinal); }
+        public override int GetInt32(int ordinal) { return reader.GetInt32(ordinal); }
+        public override short GetInt16(int ordinal) { return reader.GetInt16(ordinal); }
+        public override Guid GetGuid(int ordinal) { return reader.GetGuid(ordinal); }
+        public override float GetFloat(int ordinal) { return reader.GetFloat(ordinal); }
+        public override double GetDouble(int ordinal) { return reader.GetDouble(ordinal); }
+        public override decimal GetDecimal(int ordinal) { return reader.GetDecimal(ordinal); }
+        public override DateTime GetDateTime(int ordinal) { return reader.GetDateTime(ordinal); }
+        public override long GetChars(int ordinal, long dataOffset, char[] buffer, int bufferOffset, int length)
+        { return reader.GetChars(ordinal, dataOffset, buffer, bufferOffset, length); }
+        public override char GetChar(int ordinal) { return reader.GetChar(ordinal); }
+        public override long GetBytes(int ordinal, long dataOffset, byte[] buffer, int bufferOffset, int length)
+        { return reader.GetBytes(ordinal, dataOffset, buffer, bufferOffset, length); }
+        public override byte GetByte(int ordinal) { return reader.GetByte(ordinal); }
+        public override bool GetBoolean(int ordinal) { return reader.GetBoolean(ordinal); }
+        public override DataTable GetSchemaTable() { return reader.GetSchemaTable(); }
+        public override int GetOrdinal(string name) { return reader.GetOrdinal(name); }
+        public override string GetName(int ordinal) { return reader.GetName(ordinal); }
+        public override Type GetFieldType(int ordinal) { return reader.GetFieldType(ordinal); }
+        public override string GetDataTypeName(int ordinal) { return reader.GetDataTypeName(ordinal); }
+        public override void Close()
+        {
+            Console.WriteLine("Closing datareader...");
+            reader.Close();
+        }
+        public override int Depth { get { return reader.Depth; } }
+        public override int FieldCount { get { return reader.FieldCount; } }
+        public override int RecordsAffected { get { return reader.RecordsAffected; } }
+        public override bool IsClosed { get { return reader.IsClosed; } }
+        public override bool HasRows { get { return reader.HasRows; } }
+        public override object this[int ordinal] { get { return reader[ordinal]; } }
+        public override object this[string name] { get { return reader[name]; } }
+        public override System.Collections.IEnumerator GetEnumerator() { return reader.GetEnumerator(); }
+    }
+
     public abstract partial class ASqlWorker<TPC> : IDisposable where TPC : AbstractDbParameterConstructors, new()
     {
         protected abstract DbConnection Conn { get; }
@@ -220,18 +276,22 @@ namespace SqlWorker
         /// <returns>consequentially readed data</returns>
         virtual public IEnumerable<T> Select<T>(String command, Func<DbDataReader, T> todo, DbParametersConstructor vals = null, int? timeout = null, CommandType commandType = CommandType.Text)
         {
+            Console.WriteLine("Selecting...");
             vals = vals ?? DbParametersConstructor.emptyParams;
             SqlParameterNullWorkaround(vals);
             using (var cmd = Conn.CreateCommand())
             {
+                cmd.Disposed += (sender, e) => { Console.WriteLine("Command disposed"); };
+                Console.WriteLine("new command created...");
                 if (timeout.HasValue) cmd.CommandTimeout = timeout.Value;
                 cmd.CommandType = commandType;
                 cmd.CommandText = command;
                 cmd.Parameters.AddRange(vals);
                 cmd.Transaction = this._transaction;
                 if (this.Conn.State != ConnectionState.Open) Conn.Open();
-                using (var dr = cmd.ExecuteReader())
+                using (var dr = cmd.MyExecuteReader())
                 {
+                    Console.WriteLine("datareader created");
                     while (dr.Read())
                     {
                         yield return todo(dr);
--- Testing/Program.cs
+++ Testing/Program.cs
@@ -10,8 +10,18 @@ namespace Testing
     {
         private static void Main(string[] args)
         {
-            using (var SW = new SqlWorker.MSSqlWorker("<server>", "<database>", "<user>", "<password>")) // connecting to sql server
+            using (var SW = new SqlWorker.MSSqlWorker("G202-comp1,14331", "HOSTEL")) // connecting to sql server
             {
+                var data1 = SW.Select("select top 100 * from [log] l order by l.id desc", dr => new[] { dr[0], dr[1], dr[2], dr[3], dr[4], dr[5], dr[6], dr[7], dr[8] });
+                var x1 = data1.Take(100).ToArray();
+                var x2 = data1.Take(20).ToArray();
+                var y = data1.First();
+                foreach (var xx in data1)
+                {
+                    Console.WriteLine(String.Join("; ", xx.Select(i => i.ToString())));
+                }
+                return;
+
                 SW.TransactionBegin();
                 var n = SW.InsertValues("<the table>", new SWParameters { { "email", "ivan@example.com" }, { "msg", "Hello, world!" } }); // insert example
                 Console.WriteLine(n);
