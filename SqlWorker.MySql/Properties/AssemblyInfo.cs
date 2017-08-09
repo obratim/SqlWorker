fc427579fe19c4f86b7915197cbdda40d4036824
viktor.koryagin <none@none>
2017-08-09 12:47:12 +0300

    generating versioninfo and .numspec files for new project

--- SqlWorker.MySql/Properties/AssemblyInfo.cs
+++ SqlWorker.MySql/Properties/AssemblyInfo.cs
@@ -7,7 +7,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("SqlWorker.MySql")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SqlWorker.MySql")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
@@ -32,5 +31,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SqlWorker.MySql/Properties/VersionInfo.base.cs
@@ -0,0 +1,4 @@
+using System.Reflection;
+[assembly: AssemblyVersion("2.1.$REVNUM$.$DIRTY$")]
+[assembly: AssemblyFileVersion("2.1.$REVNUM$.$DIRTY$")]
+[assembly: AssemblyConfiguration("$BRANCH$")]
