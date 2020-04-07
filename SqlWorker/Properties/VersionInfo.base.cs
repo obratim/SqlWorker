69e4405959afe3930738f7e12682b16e2d65254b
obratim <none@none>
2020-04-07 23:51:21 +0300

    removed legacy code

--- SqlWorker/Properties/VersionInfo.base.cs
+++
@@ -1,5 +0,0 @@
-using System.Reflection;
-[assembly: AssemblyCopyright("Copyright ©  $YEAR$")]
-[assembly: AssemblyVersion("2.2.$REVNUM$.$DIRTY$")]
-[assembly: AssemblyFileVersion("2.2.$REVNUM$.$DIRTY$")]
-[assembly: AssemblyConfiguration("$BRANCH$")]
