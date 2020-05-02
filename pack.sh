8d301130173e6e73f51357e5aeb0678ba7c9d05e
obratim <none@none>
2020-05-02 19:13:38 +0300

    pack.sh script fixed

--- pack.sh
+++ pack.sh
@@ -6,11 +6,15 @@ export SW_COPYRIGHT="Copyright $(date +%Y)"
 export SW_AUTHOR="Viktor A. Koryagin"
 export SW_TAGS="sql"
 
+if [ -n "$1" ]; then
+    export SW_NOTES=$1
+fi
+
 dotnet pack \
     -p:PackageVersion="\"$SW_VERSION\"" \
     -p:PackageReleaseNotes="\"$SW_NOTES\"" \
-    -p:Copyright="\"SW_COPYRIGHT\"" \
-    -p:Authors="\"SW_AUTHOR\"" \
+    -p:Copyright="\"$SW_COPYRIGHT\"" \
+    -p:Authors="\"$SW_AUTHOR\"" \
     -p:Description="\"$SW_DESCRIPTION\"" \
     -p:PackageTags="\"$SW_TAGS\"" \
     -o publish/nuget \
