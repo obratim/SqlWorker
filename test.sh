ab149d13660c2c645514a56695c830d9389fafba
obratim <none@none>
2020-07-04 18:07:10 +0300

    test runner fix

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ echo '{
     "connectionStringMaster": "Server=localhost;Database=master;User ID=sa;Password=12345678Bb.;Integrated Security=false",
     "connectionString": "Server=localhost;Database=sqlworker_test;User ID=sa;Password=12345678Bb.;Integrated Security=false",
     "recreateDb": true
-}' > Tests/bin/Debug/netcoreapp3.1/config.json && dotnet build Test && dotnet vstest Tests/bin/Debug/netcoreapp3.1/Tests.dll
+}' > Tests/bin/Debug/netcoreapp3.1/config.json && dotnet build Tests && dotnet vstest Tests/bin/Debug/netcoreapp3.1/Tests.dll
