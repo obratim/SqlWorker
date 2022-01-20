5b3de64730003967ee84ecc9a933bcbf2e59b16e
SLAVONchick <viacheslavkoryagin@gmail.com>
2022-01-20 17:53:03 +0300

    remove blocking call

--- SqlWorker.PostgreSql/BulkCopyGeneric.cs
+++ SqlWorker.PostgreSql/BulkCopyGeneric.cs
@@ -158,7 +158,7 @@ namespace SqlWorker
                 PerformBulkCopyDataRow(writer, row, settings);
             }
 
-            writer.CompleteAsync().GetAwaiter().GetResult();
+            writer.Complete();
         }
 
 
