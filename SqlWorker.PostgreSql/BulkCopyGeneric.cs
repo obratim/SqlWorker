20739be048748ed086d7a6bcf35930329040b36c
obratim <none@none>
2022-02-13 00:30:29 +0300

    postgres bulk copy: DefaultPerformBulkCopyDataRow for case when settings == null

--- SqlWorker.PostgreSql/BulkCopyGeneric.cs
+++ SqlWorker.PostgreSql/BulkCopyGeneric.cs
@@ -18,12 +18,97 @@ namespace SqlWorker
     static class BulkCopyGeneric<T>
     {
         private static readonly Action<NpgsqlBinaryImporter, T, PostgreSqlBulkCopySettings> PerformBulkCopyDataRow;
+        private static readonly Action<NpgsqlBinaryImporter, T> DefaultPerformBulkCopyDataRow;
         private static readonly List<string> Columns;
 
         static BulkCopyGeneric()
         {
             var copyParameterWriter = Expression.Parameter(typeof(NpgsqlBinaryImporter), "writer");
             var copyParameterData = Expression.Parameter(typeof(T), "data");
+        {
+            var properties = TypeDescriptor.GetProperties(typeof(T));
+            Columns = new List<string>(properties.Count);
+
+            var writeSteps = new List<Expression>();
+            var mapperType = typeof(NpgsqlParameter).Assembly.GetType("Npgsql.TypeMapping.GlobalTypeMapper");
+            var mapper = mapperType
+                .GetProperty("Instance", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public)
+                !.GetValue(null);
+            var mappings = mapperType
+                .GetProperty("Mappings", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                !.GetValue(mapper) as Dictionary<string, Npgsql.TypeMapping.NpgsqlTypeMapping>;
+
+            foreach (PropertyDescriptor property in properties)
+            {
+                var propertyAccess = Expression.Property(copyParameterData, property.Name);
+                
+                switch (property.PropertyType)
+                {
+                    case {} when mappings!.Any(m => m.Value.ClrTypes.Contains(property.PropertyType)):
+                    {
+                        writeSteps.Add(Expression.Call(copyParameterWriter, nameof(NpgsqlBinaryImporter.Write), new [] {property.PropertyType}, propertyAccess));
+                        Columns.Add(property.Name);
+                        break;
+                    }
+                    case { IsGenericType: true } when 
+                        property.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>) && 
+                        mappings.Any(m => m.Value.ClrTypes.Contains(Nullable.GetUnderlyingType(property.PropertyType))):
+                    {
+                        var valueAccess = Expression.Property(propertyAccess, nameof(Nullable<int>.Value));
+                        
+                        writeSteps.Add(
+                            Expression.Condition(
+                                Expression.Equal(propertyAccess, Expression.Default(property.PropertyType)),
+                                Expression.Call(copyParameterWriter, typeof(NpgsqlBinaryImporter).GetMethod(nameof(NpgsqlBinaryImporter.WriteNull))),
+                                Expression.Call(copyParameterWriter, nameof(NpgsqlBinaryImporter.Write), new [] {Nullable.GetUnderlyingType(property.PropertyType)}, valueAccess)));
+                        Columns.Add(property.Name);
+                        break;
+                    }
+                    case { IsEnum: true }:
+                    {
+                        var underlyingType = Enum.GetUnderlyingType(property.PropertyType);
+                        var valueAccess = Expression.Convert(propertyAccess, underlyingType);
+                        
+                        writeSteps.Add(Expression.Call(copyParameterWriter, nameof(NpgsqlBinaryImporter.Write), new [] {underlyingType}, valueAccess));
+                        Columns.Add(property.Name);
+                        break;
+                    }
+                    case { IsGenericType: true } when 
+                        property.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>) && 
+                        Nullable.GetUnderlyingType(property.PropertyType)!.IsEnum:
+                    {
+                        var underlyingType = Enum.GetUnderlyingType(Nullable.GetUnderlyingType(property.PropertyType)!);
+                        var valueAccess = Expression.Convert(Expression.Property(propertyAccess, nameof(Nullable<int>.Value)), underlyingType);
+                        
+                        writeSteps.Add(
+                            Expression.Condition(
+                                Expression.Equal(propertyAccess, Expression.Default(property.PropertyType)),
+                                Expression.Call(copyParameterWriter, typeof(NpgsqlBinaryImporter).GetMethod(nameof(NpgsqlBinaryImporter.WriteNull))),
+                                Expression.Call(copyParameterWriter, nameof(NpgsqlBinaryImporter.Write), new [] {underlyingType}, valueAccess)));
+                        
+                        Columns.Add(property.Name);
+                        break;
+                    }
+
+                    case { IsArray: true }:
+                    {
+                        writeSteps.Add(Expression.Call(copyParameterWriter, nameof(NpgsqlBinaryImporter.Write), new[] { property.PropertyType }, propertyAccess));
+                        Columns.Add(property.Name);
+                        break;
+                    }
+                }
+            }
+
+            var body = Expression.Block(writeSteps);
+
+            var lambda = Expression.Lambda<Action<NpgsqlBinaryImporter, T>>(
+                body, 
+                copyParameterWriter,
+                copyParameterData);
+
+            DefaultPerformBulkCopyDataRow = lambda.Compile();
+        }
+        {
             var copyParameterSettings = Expression.Parameter(typeof(PostgreSqlBulkCopySettings), "settings");
 
             var properties = TypeDescriptor.GetProperties(typeof(T));
@@ -145,8 +230,7 @@ namespace SqlWorker
                 copyParameterWriter, copyParameterData, copyParameterSettings);
 
             PerformBulkCopyDataRow = lambda.Compile();
-
-            // throw new Exception(lambda.ToReadableString());
+        }
         }
 
         public static void BulkCopy(IEnumerable<T> source, NpgsqlBinaryImporter writer, PostgreSqlBulkCopySettings settings = null)
@@ -155,7 +239,10 @@ namespace SqlWorker
             foreach (var row in source)
             {
                 writer.StartRow();
-                PerformBulkCopyDataRow(writer, row, settings);
+                if (settings == null)
+                    DefaultPerformBulkCopyDataRow(writer, row);
+                else
+                    PerformBulkCopyDataRow(writer, row, settings);
             }
 
             writer.Complete();
