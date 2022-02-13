41ea07f13229eb8d609dfc13bc1c0389743ede94
obratim <none@none>
2022-02-13 13:38:14 +0300

    xml comments for MsSqlHelper

--- SqlWorker.MsSql/MsSqlHelper.cs
+++ SqlWorker.MsSql/MsSqlHelper.cs
@@ -5,8 +5,16 @@ using System.Text;
 
 namespace SqlWorker
 {
+	/// <summary>
+	/// Helpers specific to working with MS SQL Server
+	/// </summary>
     public static class MsSqlHelper
     {
+		/// <summary>
+		/// Converts Dictionary to sequence of <c cref="SqlBulkCopyColumnMapping">SqlBulkCopyColumnMapping</c>, can be used for bulk copy
+		/// </summary>
+		/// <param name="mappings">Mapping: key - name of property, value - name of DB column</param>
+		/// <returns>Sequence of <c cref="SqlBulkCopyColumnMapping">SqlBulkCopyColumnMapping</c>, can be used for bulk copy</returns>
 		public static IEnumerable<SqlBulkCopyColumnMapping> ToBulkCopyMappings(this IDictionary<string, string> mappings)
 		{
 			foreach (var kv in mappings)
