fe047ef7472169a050a4e77a32cb0753394dd5a1
obratim <none@none>
2022-02-13 13:42:53 +0300

    PostgreSql DataReaderExtensions: null check, xml comments

--- SqlWorker.PostgreSql/DataReaderExtensions.cs
+++ SqlWorker.PostgreSql/DataReaderExtensions.cs
@@ -1,12 +1,21 @@
+using System;
 using System.Data;
 
 namespace SqlWorker
 {
+	/// <summary>
+	/// Helpers specific to working with Postgres
+	/// </summary>
     public static class DataReaderExtensions
     {
+        /// <summary>
+        /// Gets array from DataReader by ordinal
+        /// </summary>
         public static T[] GetArray<T>(this IDataReader dr, int n)
         {
             var v =  dr[n];
+            if (v == null || v == DBNull.Value)
+                return null;
             return (T[]) v;
         }
     }
